5dded8387829edf177c677b7a2473b297104cc94
Jerry Chen <jerry@Jerrys-MacBook-Air.local>
2022-01-11 22:28:53 -0800

    00.00.00 save. complete but updates not tested

--- src/run_batchjob.py
+++ src/run_batchjob.py
@@ -22,6 +22,22 @@ from argparse import ArgumentParser
 
 BATCH_SETTINGS_FILE = 'batch.settings.json'
 
+
+def setJobProperties( module_name, batch_defaults_json, module_template_json):
+    job_properties = {}
+    job_properties['image'] = batch_defaults_json['ecr_registry'] + '/' + str(module_name) + ':' + str(module_template_json['module_version'])
+    job_properties['vcpus'] = int(module_template_json['compute']['vcpus']) \
+                                  if 'compute' in module_template_json and 'vcpus' in module_template_json['compute'] \
+                                  else int(batch_defaults_json['vcpus'])
+    job_properties['jobRoleArn'] = batch_defaults_json['aws_ecs_job_role']
+    job_properties['memory'] = int(module_template_json['compute']['memory']) \
+                                   if 'compute' in module_template_json and 'memory' in module_template_json['compute'] \
+                                   else int(batch_defaults_json['memory'])
+    # job_properties['mountPoints'] = batch_defaults_json['mountpoints']
+    # job_properties['volumes'] = batch_defaults_json['volumes']
+    return job_properties
+
+
 def run_batchjob( args_json ):
     
     def createDependentIdList( jobid_list ):
@@ -53,42 +69,38 @@ def run_batchjob( args_json ):
     
     # convert command-line string of arguments into an IO JSON
     io_json = module_utils.createIOJSON(args_json)
-    io_json_name = module_name+'.'+unique_id+'.io.json'
-    with open(io_json_name,'w') as fout:
-        json.dump(io_json, fout)
+    io_json_name = module_utils.getModuleRunNameID( module_name, unique_id, 'io_json' )
+    file_utils.writeJSON( io_json, io_json_name )
     print('ARGS JSON: '+str(args_json))
     
     # upload IO JSON to module directory
-    io_json_remote_folder = file_utils.uploadFile(io_json_name, os.path.join( module_utils.getModuleDirectory(), module_name, 'io/' ))
+    io_json_remote_folder = file_utils.uploadFile(io_json_name, module_utils.getModuleIODirectory( module_name ))
     io_json_remote_full_path = os.path.join(io_json_remote_folder, io_json_name)
     
     # initialize Batch boto3 client access
     print('\nSetting up boto3 client in {}...'.format(batch_defaults_json['aws_region']))
     client = boto3.client('batch', region_name=batch_defaults_json['aws_region'])
-
+    
     # initialize job jobQueue and dependent IDs
     JOB_QUEUE = args_json['jobqueue'] if 'jobqueue' in args_json and args_json['jobqueue'] != '' else batch_defaults_json['jobqueue']
     DEPENDENT_IDS = createDependentIdList(args_json['dependentid'].split(',')) if 'dependentid' in args_json and args_json['dependentid'] != '' else []
-
+    
     # set properties for this job
-    job_properties = {}
-    job_properties['image'] = batch_defaults_json['ecr_registry'] + '/' + str(module_name) + ':' + str(module_template_json['module_version'])
-    job_properties['vcpus'] = int(module_template_json['compute']['vcpus']) \
-                                  if 'compute' in module_template_json and 'vcpus' in module_template_json['compute'] \
-                                  else int(batch_defaults_json['vcpus'])
-    job_properties['jobRoleArn'] = batch_defaults_json['aws_ecs_job_role']
-    job_properties['memory'] = int(module_template_json['compute']['memory']) \
-                                   if 'compute' in module_template_json and 'memory' in module_template_json['compute'] \
-                                   else int(batch_defaults_json['memory'])
-    # job_properties['mountPoints'] = batch_defaults_json['mountpoints']
-    # job_properties['volumes'] = batch_defaults_json['volumes']
-    job_name = 'job_'+str(module_name)+'_'+unique_id
+    job_properties = setJobProperties( module_name, batch_defaults_json, module_template_json )
+    job_name = module_utils.getModuleRunNameID( module_name, unique_id, 'job' )
     print('Setting job properties for job: '+str(job_name))
 
-    # set input parameters for job submission
+    # set input and compute parameters for job submission - save this job submission info
     job_overrides = {'command': setContainerOverrides(batch_defaults_json['working_dir'], module_name, io_json_remote_full_path)}
+    job_json = job_overrides
+    job_json['jobqueue'] = JOB_QUEUE
+    job_json['jobname'] = job_name    
+    job_json_name = module_utils.getModuleRunNameID( module_name, unique_id, 'job_json' )
+    file_utils.writeJSON( job_json, job_json_name )
+    job_json_remote_folder = file_utils.uploadFile(job_json_name, module_utils.getModuleJobDirectory( module_name ))
+    job_json_remote_fullpath = os.path.join(job_json_remote_folder, job_json_name)
     
-    job_def_name = 'jdef_'+str(module_name)+'_'+unique_id
+    job_def_name = module_utils.getModuleRunNameID( module_name, unique_id, 'job_def' )
     jobid_final = ''    
     if not module_utils.isDryRun( args_json ):
         # register job definition    
@@ -97,7 +109,7 @@ def run_batchjob( args_json ):
                                                            retryStrategy={'attempts': 3},
                                                            containerProperties=job_properties)
         print('\nRegistering Job Definition: '+str(job_def_name))
-
+        
         # submit job                                                                                             
         job_submit_response = client.submit_job( jobName = job_name,
                                                  jobQueue = JOB_QUEUE,
