a3f8419f779a801869a11f0bb4d3c9f2da2c169c
EC2 Default User <ec2-user@ip-10-88-1-53.us-west-2.compute.internal>
2022-07-18 20:41:08 +0000

    MVP-23 lambda_run_pipeline and lambda_run_batchjob work via API gateway POST requests

--- run_batchjob_lambda/app/app.py
+++ run_batchjob_lambda/app/app.py
@@ -4,26 +4,30 @@ import lambda_utils
 
 def lambda_handler(event, context):
 
-    # parse POST body within event object - assume parameters are in 'data' key
-    input_json = {}    
-    input_json['module'] = lambda_utils.getParameter( event['data'], 'module', 'goqc' )
-    input_json['input'] = lambda_utils.getParameter( event['data'], 'input', 's3://hubseq-data/test/rnaseq/run_test1/david_go/davidgo.goterms.txt' )
-    input_json['output'] = lambda_utils.getParameter( event['data'], 'output', 's3://hubseq-data/test/rnaseq/run_test1/goqc/')
+    # print input request
+    print('INPUT EVENT: '+str(event))
+    
+    # parse POST body within event object - assume parameters are in 'body' key
+    input_json = {}
+    event_body = json.loads(event['body'])
+    input_json['module'] = lambda_utils.getParameter( event_body, 'module', 'goqc' )
+    input_json['input'] = lambda_utils.getParameter( event_body, 'input', 's3://hubseq-data/test/rnaseq/run_test1/david_go/davidgo.goterms.txt' )
+    input_json['output'] = lambda_utils.getParameter( event_body, 'output', 's3://hubseq-data/test/rnaseq/run_test1/goqc/')
 
     # for now, instead of throwing an error with missing input parameters, return mock run output. Can have mock run output indicate that parameters are missing.
-    if 'module' not in event['data'] or 'input' not in event['data'] or 'output' not in event['data']:
+    if 'module' not in event_body or 'input' not in event_body or 'output' not in event_body:
         input_json['mock'] = True
         input_json['dryrun'] = True
 
-    input_json['scratchdir'] = lambda_utils.getParameter( event['data'], 'scratchdir', '/tmp/' )
+    input_json['scratchdir'] = lambda_utils.getParameter( event_body, 'scratchdir', '/tmp/' )
 
     # include optional parameters in batch job call, if present
     optional_params = ['sampleid', 'program_subname', 'inputdir', 'outputdir', 'pargs', 'alternate_inputs', \
                        'alternate_outputs', 'dependentid', 'jobqueue']
 
     for param in optional_params:
-        if param in event['data']:
-            input_json[param] = lambda_utils.getParameter( event['data'], param, '' )
+        if param in event_body:
+            input_json[param] = lambda_utils.getParameter( event_body, param, '' )
     
     # input_json['module'] = "goqc"
     # input_json['input'] = "s3://hubseq-data/test/rnaseq/run_test1/david_go/davidgo.goterms.txt"
--- run_pipeline_lambda/app/app.py
+++ run_pipeline_lambda/app/app.py
@@ -1,24 +1,47 @@
 import json
 import run_pipeline
+import lambda_utils
 
 def lambda_handler(event, context):
 
     input_json = {}
-    input_json['pipeline'] = "rnaseq:mouse"
-    input_json['modules'] = "deseq2,deqc,david_go,goqc"
-    input_json['input'] = "s3://hubtenants/hubseq/test/runs/test-20220714-1633/expressionqc/expressionqc.counts_matrix.column.csv"
-    input_json['altinputs'] = "'s3://hubtenants/<team_id>/<user_id>/runs/<run_id>/expressionqc/expressionqc.samplegroups.csv','','',''"
-    input_json['moduleargs'] = "'','-pvaluecolumn pvalue','-cond pvalue<0.5,log2FoldChange>0.5',''"
-    input_json['teamid'] = "hubseq"
-    input_json['userid'] = "test"
-    input_json['runid'] = "test-20220714-1633"
-    input_json['scratchdir'] = '/tmp/'        
+    event_body = json.loads(event_body)
+    input_json['pipeline'] = lambda_utils.getParameter( event_body, 'pipeline', 'rnaseq:mouse' )
+    input_json['teamid'] = lambda_utils.getParameter( event_body, 'teamid', 'test' )
+    input_json['userid'] = lambda_utils.getParameter( event_body, 'userid', 'test' )
+    input_json['modules'] = lambda_utils.getParameter( event_body, 'modules', 'fastqc' )
+    input_json['input'] = lambda_utils.getParameter( event_body, 'input', 's3://hubtenants/test/rnaseq/run_test1/fastq/rnaseq_mouse_test_tiny1_R1.fastq.gz,s3://hubtenants/test/rnaseq/run_test1/fastq/rnaseq_mouse_test_tiny1_R2.fastq.gz' )
+    
+    required_params = ['pipeline', 'teamid', 'userid', 'modules', 'input']
+    for param in required_params:
+        if param not in event_body:
+            input_json['mock'] = True
+            input_json['dryrun'] = True
+    
+    input_json['scratchdir'] = lambda_utils.getParameter( event_body, 'scratchdir', '/tmp/' )
+
+    # include optional parameters in batch job call, if present
+    optional_params = ['runid', 'moduleargs', 'altinputs', 'altoutputs', 'jobqueue', 'output']
+    
+    for param in optional_params:
+        if param in event_body:
+            input_json[param] = lambda_utils.getParameter( event_body, param, '' )
+    
+    # input_json['pipeline'] = "rnaseq:mouse"
+    # input_json['modules'] = "deseq2,deqc,david_go,goqc"
+    # input_json['input'] = "s3://hubtenants/hubseq/test/runs/test-20220714-1633/expressionqc/expressionqc.counts_matrix.column.csv"
+    # input_json['altinputs'] = "'s3://hubtenants/<team_id>/<user_id>/runs/<run_id>/expressionqc/expressionqc.samplegroups.csv','','',''"
+    # input_json['moduleargs'] = "'','-pvaluecolumn pvalue','-cond pvalue<0.5,log2FoldChange>0.5',''"
+    # input_json['teamid'] = "hubseq"
+    # input_json['userid'] = "test"
+    # input_json['runid'] = "test-20220714-1633"
+    # input_json['scratchdir'] = '/tmp/'        
     # input_json['mock'] = True
     # input_json['dryrun'] = True
     
     json_out = run_pipeline.run_pipeline(input_json)
     
-    message = 'Hello from Lambda! Jerry is here. Here are job IDs: {}'.format(str(json_out))
+    message = 'Hello from Lambda! Jerry is here. Here is the input JSON: {}. Here are job IDs: {}'.format(str(input_json), str(json_out))
     message_response = json.dumps({'message': message})
     print(message)
     
