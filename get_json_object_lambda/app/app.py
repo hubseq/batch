122bc0213788b476883971a7c3aa4271b55f738a
EC2 Default User <ec2-user@ip-10-88-1-53.us-west-2.compute.internal>
2022-07-20 01:53:57 +0000

    MVP-54 get_json_object_lambda done

---
+++ get_json_object_lambda/app/app.py
@@ -0,0 +1,26 @@
+import json
+import aws_s3_utils
+import lambda_utils
+
+def lambda_handler(event, context):
+
+    # print input request
+    print('INPUT EVENT: '+str(event))
+    
+    # parse POST body within event object - assume parameters are in 'body' key
+    input_json = {}
+    event_body = json.loads(event['body'])
+    input_json['objects'] = lambda_utils.getParameter( event_body, 'objects', '' )
+    json_out = aws_s3_utils.get_json_object(input_json['objects'])
+    
+    message = 'Hello from Lambda! Get metadata. Jerry is here. Here was the call: {}. Here is the metadata: {}'.format(str(input_json), str(json_out))
+    message_response = json.dumps({'message': message})
+    print(message)
+    
+    response_obj = {}
+    response_obj['statusCode'] = 200
+    response_obj['headers'] = {}
+    response_obj['headers']['Content-Type'] = 'application/json'
+    response_obj['body'] = message_response
+    
+    return response_obj
